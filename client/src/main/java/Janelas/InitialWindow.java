badddc79252dde74560c12fc62f037fa3c2b8d4f
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-07-31 16:25:00 -0300

    tratamento de erros

--- client/src/main/java/Janelas/InitialWindow.java
+++ client/src/main/java/Janelas/InitialWindow.java
@@ -42,10 +42,10 @@ public class InitialWindow {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
-                    short ra = Short.parseShort(RATextField.getText());
-                    int codigo = Integer.parseInt(CodigoDaDisciplinaTextField.getText());
-                    double nota = Double.parseDouble(notaTextField.getText());
-                    double frequencia = Double.parseDouble(frequenciaTextField.getText());
+                    short ra = getRa(RATextField.getText());
+                    int codigo = getCod(CodigoDaDisciplinaTextField.getText());
+                    double nota = getNota(notaTextField.getText());
+                    double frequencia = getFreq(frequenciaTextField.getText());
                     limparCampos();
                     Resultado novoResultado = new Resultado(ra, codigo, nota, frequencia);
                     filaResultado.guardeUmItem(novoResultado);
@@ -77,5 +77,53 @@ public class InitialWindow {
         this.CodigoDaDisciplinaTextField.setText("");
         this.nomeDoAlunoTextField.setText("");
         this.notaTextField.setText("");
+    }
+
+    private short getRa(String raStr) throws Exception {
+        try {
+            short ret = Short.parseShort(raStr);
+            return ret;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "RA inválido");
+            RATextField.setText("");
+            RATextField.grabFocus();
+            throw new Exception("Erro ao converter RA");
+        }
+    }
+
+    private int getCod(String codStr) throws Exception {
+        try {
+            int ret = Integer.parseInt(codStr);
+            return ret;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Código de disciplina inválido inválido");
+            CodigoDaDisciplinaTextField.setText("");
+            CodigoDaDisciplinaTextField.grabFocus();
+            throw new Exception("Erro ao converter COD de Disciplina");
+        }
+    }
+
+    private double getNota(String notaStr) throws Exception {
+        try {
+            double ret = Integer.parseInt(notaStr);
+            return ret;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Nota inválida");
+            notaTextField.setText("");
+            notaTextField.grabFocus();
+            throw new Exception("Erro ao converter a nota");
+        }
+    }
+
+    private double getFreq(String freqStr) throws Exception {
+        try {
+            double ret = Integer.parseInt(freqStr);
+            return ret;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Frequencia inválida");
+            frequenciaTextField.setText("");
+            frequenciaTextField.grabFocus();
+            throw new Exception("Erro ao converter COD de Disciplina");
+        }
     }
 }
